b9cfa1a0e76cca7fb5b0eb795dcedc4f723ce493
Morgan Jackson <morjack@siue.edu>
2023-04-19 00:11:13 -0500

    resume fix for mobile

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -5,7 +5,7 @@ import StandardPage from "../components/wrappers/StandardPage";
 export default function Resume() {
   return (
     <StandardPage>
-      <object data={resume} className="w-full h-full">
+      <object data={resume} className="w-full h-full object-contain">
         <div className="flex flex-col justify-center text-center">
           <p>Oops, looks like your browser doesn't support PDFs!</p>
           <p><a href={resume}>Download Instead</a></p>
